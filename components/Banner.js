477a3853329750066af543bd8b896f9423d9685d
Chris <chris.conner57@gmail.com>
2021-02-28 19:42:54 -0800

    Updated logo

--- components/Banner.js
+++ components/Banner.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const Banner = () => {
 	return (
 		<View style={styles.header}>
-			<StatusBar backgroundColor='hsl(229, 17%, 13%)' barStyle='light-content' />
-			<Text style={styles.title}>Movie Atlas</Text>
+			<StatusBar
+				backgroundColor='hsl(229, 17%, 13%)'
+				barStyle='light-content'
+			/>
+			<Image
+				style={styles.logo}
+				source={ require('../assets/Movie-Atlas-Logo.png')}
+			/>
 			<Ionicons name='search' size={28} color='hsl(0,0%,100%)' />
 		</View>
 	);
@@ -18,16 +24,16 @@ const styles = StyleSheet.create({
 	header: {
 		height: 100,
 		paddingTop: 40,
-		paddingHorizontal: 30,
+		paddingLeft: 5,
+		paddingRight: 20,
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'space-between',
 		borderBottomWidth: 1,
 		backgroundColor: 'hsl(229, 17%, 13%)',
 	},
-	title: {
-		fontSize: 24,
-		fontFamily: 'Jost',
-		color: 'hsl(0,0%,100%)',
+	logo: {
+		height: 25,
+		width: 220,
 	},
 });
